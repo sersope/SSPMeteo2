f2f97cb077de29c27046e904b762b43649d55d68
sersope <sersope@gmail.com>
2016-07-14 19:10:13 +0200

    Modificaciones para WEMOS en SSPMeteo2.ino

--- Arduino/SSPMeteo2/SSPMeteo2.ino
+++ Arduino/SSPMeteo2/SSPMeteo2.ino
@@ -1,46 +1,43 @@
  /*
-    Codificación de datos:
-    -   nº del mensaje [1..255] 8 bits
-    // TODO (sergio#1#): Determinar códigos de error para los valores erróneos
-    -   temperatura [-30,0ºC, 70,0ºC] float    -> codigo temperatura= word(temperatura * 10) + 300 [0,1000]  Error de lectura:
-    -   humedad     [0.0%, 100.0%]    float    -> codigo humedad= word(humedad * 10)               [0,1000]. Error de lectura:
-    -   tics lluvia [0, 65535] 16 bits. word
-    -   velocidad viento [0.0, 160.0 km/h]     float    -> codigo vel. vent. = word(vel_vent * 10)
-    -   velocidad racha  [0.0, 160.0 km/h]     float    -> codigo vel. racha = word(vel_racha *10)
-    -   direccion viento [0, 359]     word   
-    mensaje1: unsigned long. 32 bits
-        0000000000000001tttttttttttttttt -> temperatura.
-    mensaje2: unsigned long. 32 bits
-        0000000000000010hhhhhhhhhhhhhhhh -> humedad.
-    mensaje3: unsigned long. 32 bits
-        0000000000000011rrrrrrrrrrrrrrrr -> lluvia.
-    mensaje4: unsigned long. 32 bits
-        0000000000000100rrrrrrrrrrrrrrrr -> velocidad del viento.
-    mensaje5: unsigned long. 32 bits
-        0000000000000101rrrrrrrrrrrrrrrr -> velocidad de racha
-    mensaje6: unsigned long. 32 bits
-        0000000000000110rrrrrrrrrrrrrrrr -> direccion del viento
+  SSPMeteo2 para WEMOS D1 R2
+  
+  The MIT License (MIT)
+
+  Copyright (c) 2016 Sergio Soriano Peiró
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
 */
 
-#include <RCSwitch.h>
 #include "DHT.h"
 
+// TODO Los pines se deben redefinir o usar los correpondientes en WEMOS
 #define LLUVIA_PIN  2
 #define LLUVIA_INT  0
 #define ANEMOM_PIN  3
 #define ANEMOM_INT  1
 #define ANEMOM_FACTOR 2.4     // km/h para un giro de un segundo. DETERMINAR EXPERIMENTALMENTE PARA NUEVO ANEMOMETRO
 #define DHT22_PIN   4
-#define RF433_PIN   7
 #define VELETA_PIN  A0
 
 #define INTERVALO_BASE 10000  // (en milisegundos) 
 #define NUM_INTERVALOS 5      // Número de intervalos base que pasan en cada lectura/trasmision de valores (Ej. 5 * 10s. = 50seg.)
-#define N_MENSAJES  6         // Número total de mensajes a transmitir
 
-// TRANSMISOR RF433
-RCSwitch rcswitch = RCSwitch();
-long mensaje[N_MENSAJES];  // Contiene los mensajes a transmitir
 
 // DHT22: TEMPERATURA Y HUMEDAD
 DHT dht(DHT22_PIN, DHT22);  // Initialize DHT sensor for normal 16mhz Arduino
@@ -148,42 +145,11 @@ void sendData()
 }
 
 
-// Codifica los mensajes segun los valores a transmitir
-void codifica()
-{
-  word ct,ch,cvv,cvr;
-  ct = (word)round(temp * 10.0) + 300;
-  ch = (word)round(humi * 10.0);
-  cvv = (word)round(vel_vent * 10.0);
-  cvr = (word)round(vel_racha * 10.0);
-  mensaje[0] = 0x10000 | ct;
-  mensaje[1] = 0x20000 | ch;
-  mensaje[2] = 0x30000 | lluvia_ticks;
-  mensaje[3] = 0x40000 | cvv;
-  mensaje[4] = 0x50000 | cvr;
-  mensaje[5] = 0x60000 | dir_vent;
-}
- 
-// Transmitir los mensajes en secuencia por RF4333
-void transmite()
-{
-  digitalWrite(13,HIGH);
-  codifica();
-  int i = 0;
-  for (i = 0; i < N_MENSAJES; i++)
-  {
-    rcswitch.send(mensaje[i], 24);
-    delay(50);
-  }
-  digitalWrite(13,LOW);
-}
-
-
  
 void setup()
 {
-  //Serial.begin(9600);
-  //Serial.println("SSPMeteo.");
+  Serial.begin(9600);
+  Serial.println("SSPMeteo2.");
     
   // Para el conteo de lluvia
   pinMode(LLUVIA_PIN,INPUT_PULLUP);
@@ -195,12 +161,7 @@ void setup()
   
   // Prepara el sensor DHT22
   dht.begin();
-  
-  // Prepara la transmision RF433
-  pinMode(13,OUTPUT);  // Este LED durante la transmision
-  rcswitch.enableTransmit(RF433_PIN);
-  rcswitch.setRepeatTransmit(20);
-  
+    
   // Primera vez
   timer_lectura = millis();
   intervalo = 0;
@@ -217,8 +178,7 @@ void loop()
     {
       leeVeleta();
       leeDHT();
-      //sendData();
-      transmite();
+      sendData();
       
       intervalo = 0;
       vel_vent = 0.0;
