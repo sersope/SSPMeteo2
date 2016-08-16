28270bac0ae51acc3d18072b3d298d0518d2b10b
sersope <sersope@gmail.com>
2016-08-16 16:53:09 +0200

    Funcionalidad al 100% 	- Añadido el sensor BME280 	- Implementada toda la gestion de la lluvia 	- Envio de datos a servidor

--- Arduino/SSPMeteo2/SSPMeteo2.ino
+++ Arduino/SSPMeteo2/SSPMeteo2.ino
@@ -1,12 +1,3 @@
-
-
-// TODO:
-//        Resetear lluvia diaria por cambio de dia. HECHO
-//        Contemplar lluvia última hora.
-//        Calcular punto de rocio. HECHO
-//        Meter las conversiones de unidades en una sola subrutina. HECHO
-//        Completar el envio a Wunder.
-
 /*
   SSPMeteo2 para WEMOS D1 R2
 
@@ -33,45 +24,45 @@
   SOFTWARE.
 */
 
-#include "DHT.h"
 #include <Wire.h>
-#include <Adafruit_BMP085.h>
+#include <Adafruit_BME280.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <QueueList.h>
 
 // Los pines usados
-#define LLUVIA_PIN  D3
+#define LLUVIA_PIN  D7
 #define ANEMOM_PIN  D6
-#define DHT22_PIN   D5
 #define VELETA_PIN  A0
 
-#define ANEMOM_FACTOR 2.4     // km/h para un giro de un segundo. Para anemometro WH1080.
-#define LLUVIA_FACTOR 0.138   // Por cada tick de mi pluviómetro. En mm.
-#define ALTITUD 20            // En mi casa. en metros.
+#define ANEMOM_FACTOR 2.4       // km/h para un giro de un segundo. Para anemometro WH1080.
+#define LLUVIA_FACTOR 0.138     // Por cada tick de mi pluviómetro. En mm.
+#define ALTITUD 20.0            // En mi casa. en metros.
 
-#define INTERVALO_BASE 60000   // (en milisegundos) 
-#define NUM_INTERVALOS 10      // Número de intervalos base que pasan en cada lectura/trasmision de valores (Ej. 5 * 10s. = 50seg.)
+#define INTERVALO_BASE 60000    // (en milisegundos) 
+#define NUM_INTERVALOS 10       // Número de intervalos base que pasan en cada lectura/trasmision de valores (Ej. 5 * 10s. = 50seg.)
 
+const uint16_t puerto = 1234;
+const char * servidor = "192.168.1.10"; // ip or dns
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org");
 int hora_actual;
 
-// BMP: PRESION Y TEMPERATURA
-Adafruit_BMP085 bmp;
-float temp2; // Temperatura del BMP en ºC
+// BME280: PRESION, TEMPERATURA Y HUMEDAD
+Adafruit_BME280 bme; // I2C
+float temp; // Temperatura del BMP en ºC
 float pres;  // Presion en mbar
-
-// DHT22: TEMPERATURA Y HUMEDAD
-DHT dht(DHT22_PIN, DHT22);
 float humi;  // Huemdad relativa en %
-float temp;  // Temperatura en ºC
 float troc;  // Punto de rocio en ºC
+
 // LLUVIA
-float lluvia_dia;                            // Lluvia diaria en inches
+float lluvia_dia;                            // Lluvia diaria
+float lluvia_last_intervalo;                           // Acumulado de lluvia en la ultima hora
 volatile word lluvia_ticks = 0L;             // variables modified in callback must be volatile
 volatile unsigned long lluvia_last = 0UL;    // the last time the output pin was toggled
+QueueList <word> lluvia_cola;                // Cola FIFO para la gestión de la lluvia ultimo intervalo(s)
 
 // ANEMOMETRO
 float vel_vent;         // Velocidad del viento en km/h
@@ -108,25 +99,15 @@ void cuenta_anemom()
   anemom_last = millis();
 }
 
-// Obten valores de Temperatura y Humedad
-void leeDHT()
+// Obten valores de presion y temperatura del BMP
+void leeBME()
 {
-  float auxhumi = dht.readHumidity();
-  float auxtemp = dht.readTemperature(); // En ºC
-  // Si algun fallo en la lectura manten el valor actual
-  if (!isnan(auxhumi) && !isnan(auxtemp))
-  {
-    humi = auxhumi;
-    temp = auxtemp;
-  }
+  temp = bme.readTemperature();                       // En ºC
+  pres = bme.readPressure() / 100;                    // En mbar
+  pres = (pres / pow(1.0 - ALTITUD / 44330, 5.255));  // At sea level
+  humi = bme.readHumidity();
+  // Punto de rocío
   troc = pow(humi / 100.0, 1.0 / 8.0) * (112.0 + 0.9 * temp) + 0.1 * temp - 112.0;
-}
-
-// Obten alores de presion y temperatura del BMP
-void leeBMP()
-{
-  temp2 = bmp.readTemperature();              // En ºC
-  pres = bmp.readSealevelPressure(ALTITUD) / 100; // En mbar
 }
 
 // Obten la velocidad del viento como promedio en el intervalo NUM_INTERVALOS * INTERVALO_BASE
@@ -163,23 +144,19 @@ void leeVeleta()
   dir_vent = veletaDir[min_i];
 }
 
-void printSerial()
+void leeLluvia()
 {
   lluvia_dia = lluvia_ticks * LLUVIA_FACTOR;
-  Serial.print(timeClient.getFormattedTime());
-  Serial.print(" Temp= ");   Serial.print(temp, 1);
-  Serial.print(" Hume= ");   Serial.print(humi, 0);
-  Serial.print(" Troc= ");   Serial.print(troc, 1);
-  Serial.print(" Tem2= ");   Serial.print(temp2, 1);
-  Serial.print(" Pres= ");   Serial.print(pres);
-  Serial.print(" Lluv= ");   Serial.print(lluvia_dia, 3);
-  Serial.print(" (");        Serial.print(lluvia_ticks);
-  Serial.print(") Vven= ");  Serial.print(vel_vent, 1);
-  Serial.print(" Vrac= ");   Serial.print(vel_racha, 1);
-  Serial.print(" Dven= ");   Serial.println(dir_vent);
+  // Gestion lluvia ultimo(s) intervalo(s)
+  lluvia_cola.push(lluvia_ticks);
+  lluvia_last_intervalo = (lluvia_ticks - lluvia_cola.peek()) * LLUVIA_FACTOR;
+  if(lluvia_cola.count() > 1) // Lluvia en los ultimos n intervalos
+  {
+    lluvia_cola.pop();
+  }
 }
 
-void comunicaPorWifi(bool wunder = false)
+void comunicaPorWifi(bool send_to_wunder = false, bool send_to_servidor= true)
 {
   const char* ssid     = "Wsergio";
   const char* password = "cochin29";
@@ -204,15 +181,43 @@ void comunicaPorWifi(bool wunder = false)
     Serial.print(WiFi.status());
     Serial.print(" IP:");
     Serial.println(WiFi.localIP());
-    if (wunder)
+    // Envio al servidor
+    if (send_to_servidor)
     {
-      // Acceso a Wunder
+      WiFiClient cliente;
+      Serial.print(timeClient.getFormattedTime());
+      Serial.print(" Servidor "); Serial.print(servidor);
+      if (!cliente.connect(servidor, puerto)) 
+      {
+        Serial.println(": Conexión fallida.");
+      }
+      else
+      {
+        cliente.print(temp, 1);
+        cliente.print(", ");cliente.print(humi, 0);
+        cliente.print(", ");cliente.print(troc, 1);
+        cliente.print(", ");cliente.print(pres,1);
+        cliente.print(", ");cliente.print(lluvia_dia, 1);
+        cliente.print(", ");cliente.print(lluvia_last_intervalo, 1);
+        cliente.print(", ");cliente.print(vel_vent, 1);
+        cliente.print(", ");cliente.print(vel_racha, 1);
+        cliente.print(", ");cliente.print(dir_vent);
+        cliente.print('Q');
+        cliente.stop();
+        Serial.println(": Datos enviados");
+      }
+    }
+    // Acceso a Wunder
+    if (send_to_wunder)
+    {
+      convertirUnidades(); // A Wunder unidades imperiales
       HTTPClient http;
-      String uri = "http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID=ICOMUNID54&PASSWORD=laura11&action=updateraw&dateutc=now";
+      String uri = String("http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?ID=ICOMUNID54&PASSWORD=laura11&action=updateraw&dateutc=now");
       uri += "&tempf=";        uri += temp;
       uri += "&humidity=";     uri += humi;
       uri += "&dewptf=";       uri += troc;
       uri += "&dailyrainin=";  uri += lluvia_dia;
+      uri += "&rainin=";       uri += lluvia_last_intervalo;
       uri += "&windspeedmph="; uri += vel_vent;
       uri += "&windgustmph=";  uri += vel_racha;
       uri += "&winddir=";      uri += dir_vent;
@@ -231,7 +236,7 @@ void comunicaPorWifi(bool wunder = false)
       }
       else
       {
-        Serial.printf("error: %s\n", http.errorToString(httpCode).c_str());
+        Serial.print("error: "); Serial.println(http.errorToString(httpCode).c_str());
       }
       http.end();
     }
@@ -258,6 +263,14 @@ void comunicaPorWifi(bool wunder = false)
     Serial.print(" Estado:");
     Serial.println(WiFi.status());
   }
+  else
+  {
+    Serial.print(timeClient.getFormattedTime());
+    Serial.print(" WiFi desconectada en ");
+    Serial.print((millis() - ini) / 1000.0, 3);
+    Serial.print(" Estado:");
+    Serial.println(WiFi.status());
+  }
   Serial.println("");
 }
 
@@ -275,13 +288,13 @@ void desconectaInterrupts()
 
 void convertirUnidades()
 {
-  temp = dht.convertCtoF(temp);
-  temp2 = dht.convertCtoF(temp2);
-  troc = dht.convertCtoF(troc);
-  pres *= 0.0295299830714; // A pulgadas de columna de mercurio
-  lluvia_dia = lluvia_ticks * LLUVIA_FACTOR / 25.4; // En pulgadas
-  vel_vent *=  0.621371192; // En mph
-  vel_racha *= 0.621371192; // En mph
+  temp = temp * 1.8 + 32;         // to ºF
+  troc = troc * 1.8 + 32;
+  pres *= 0.0295299830714;        // A pulgadas de columna de mercurio
+  lluvia_dia /= 25.4;             // En pulgadas
+  lluvia_last_intervalo /= 25.4;  // En pulgadas
+  vel_vent *=  0.621371192;       // En mph
+  vel_racha *= 0.621371192;       // En mph
 }
 
 void setup()
@@ -292,18 +305,19 @@ void setup()
   Serial.println();
   Serial.println("SSPMeteo2.");
   Serial.println();
-  comunicaPorWifi(false); //Solo para obtener la hora
+  comunicaPorWifi(false, false); //Solo para obtener la hora
   // Para el conteo de lluvia
   pinMode(LLUVIA_PIN, INPUT_PULLUP);
   // Para el conteo del anemometro
   pinMode(ANEMOM_PIN, INPUT_PULLUP);
   conectaInterrupts();
-  // Prepara el sensor DHT22
-  pinMode(DHT22_PIN, INPUT_PULLUP);
-  dht.begin();
-  // Prepara el sensor BMP
-  bmp.begin();
+  // Prepara el sensor BME280
+  if (!bme.begin(0x76))
+  {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+  }
   // Primera vez
+  lluvia_cola.push(lluvia_ticks);
   timer_lectura = millis();
   intervalo = 0;
   hora_actual = timeClient.getHours();
@@ -320,14 +334,12 @@ void loop()
     if (intervalo == NUM_INTERVALOS)
     {
       // Lectura de sensores
+      leeLluvia();
       leeVeleta();
-      leeDHT();
-      leeBMP();
+      leeBME();
       // Comunicate
-      printSerial();
-      convertirUnidades();
       desconectaInterrupts();
-      comunicaPorWifi(true);// FALSE:evita acceso a wunder (PARA PRUEBAS)
+      comunicaPorWifi(false, true);// FALSE:evita acceso a wunder (PARA PRUEBAS)
       conectaInterrupts();
       // Comprueba cambio de dia
       hora = timeClient.getHours();
