cc6aea9efd1862b47a62cdcf4e41e47a83e7e7de
Sergio Soriano Peiró <sersope@gmail.com>
2016-10-04 13:41:55 +0200

    Rediseño casi total del código: - La clase gestora de la pantalla se convierte es clase estática (variables y metodos de clase) - El servidor se implementa con la librería estándard socketserver

--- Raspi/sspmeteo2_server.py
+++ Raspi/sspmeteo2_server.py
@@ -1,8 +1,5 @@
-import select
-import socket
-import sys
+import socketserver
 import logging
-from datetime import datetime
 from oled.device import sh1106
 from oled.render import canvas
 from PIL import ImageDraw, ImageFont
@@ -10,27 +7,33 @@ from PIL import ImageDraw, ImageFont
 FONT_FILE0 = 'Roboto-BoldCondensed.ttf'
 FONT_FILE1 = 'wwDigital.ttf'
 
-class MiPantallita:
-    def __init__(self):
-        self.oled = sh1106()
-        self.font0 = ImageFont.truetype(FONT_FILE0, 30)
-        self.font1 = ImageFont.truetype(FONT_FILE0, 24)
-        self.font2 = ImageFont.truetype(FONT_FILE1, 12)
-        with canvas(self.oled) as draw:
-            draw.text((8, 14), 'SSPMeteo2', 1, self.font1)
-            draw.text((0, 50), 'Esperando datos...', 1, self.font2)
+class MiOled:
+    
+    oled = sh1106()
+    font0 = ImageFont.truetype(FONT_FILE0, 30)
+    font1 = ImageFont.truetype(FONT_FILE0, 24)
+    font2 = ImageFont.truetype(FONT_FILE1, 12)
+    
+    @classmethod
+    def begin(cls):
+        with canvas(cls.oled) as draw:
+            draw.text((8, 14), 'SSPMeteo2', 1, cls.font1)
+            draw.text((0, 50), 'Esperando datos...', 1, cls.font2)
 
-    def update(self, datos):
-        lval = datos.split(', ')
-        val = [float(x) for x in lval[1:]]     # Remove wemos date (lval[0])
-        #~ status = datetime.now().strftime('%X') + '   '
-        with canvas(self.oled) as draw:
+    @classmethod
+    def update(cls, datos):
+        try:
+            lval = datos.split(', ')
+            val = [float(x) for x in lval[1:]]     # Remove wemos date (lval[0])
+        except:
+            return
+        with canvas(cls.oled) as draw:
             # Line 1 - Temp. and humidity
             line = '{:.1f}º {:.0f}%'.format(val[0],val[1])
-            font = self.font0
-            if draw.textsize(line, font)[0] > self.oled.width:
-                font = self.font1
-            of = int((self.oled.width - draw.textsize(line, font)[0]) / 2)
+            font = cls.font0
+            if draw.textsize(line, font)[0] > cls.oled.width:
+                font = cls.font1
+            of = int((cls.oled.width - draw.textsize(line, font)[0]) / 2)
             draw.text((0 + of, 0), line, 1, font)
             
             # Line 2 - Rain
@@ -41,54 +44,27 @@ class MiPantallita:
                     #~ status = '¡ LLUEVE !   '
                 else:
                     line = 'Lluvia diaria {:.0f}mm'.format(val[4])
-            draw.text((0, 28), line, 1, self.font2)
+            draw.text((0, 28), line, 1, cls.font2)
             
             # Line 3 - Pressure and wind
             line = '{:.0f}mb {:.0f}kph {:.0f}º'.format(val[3],val[6],val[8])
-            font = self.font2
+            font = cls.font2
             draw.text((0, 40), line, 1, font)
             
             # Line 4 - Status
             d, resto = divmod(val[9] * 5, 24 * 60)
             h, m = divmod(resto, 60)
             line = '{} {}d{}:{}'.format(lval[0], int(d), int(h), int(m))
-            draw.text((0, 52), line, 1, self.font2)
-    
-class Server:
-    def __init__(self, port):
-        self.host = ''
-        self.port = port
-        self.server = None
+            draw.text((0, 52), line, 1, cls.font2)
 
-    def open_server(self):
-        try:
-            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.server.bind((self.host,self.port))
-            self.server.listen(5)
-            logging.info('A la escucha...')
-        except socket.error as error:
-            if self.server:
-                self.server.close()
-            logging.info("No se puede abrir el socket: " + error.strerror)
-            sys.exit(1)
 
-    def run(self, pantallita):
-        self.open_server()
-        input = [self.server]
-        running = True
-        while running:
-            inputready,outputready,exceptready = select.select(input,[],[])
-            for s in inputready:
-                # handle the server socket
-                if s == self.server:
-                    cliente, foo = self.server.accept()
-                    datos = cliente.recv(1024).decode()
-                    if 'Q' in datos:
-                        datos = datos.replace('Q','')
-                        logging.info(datos)
-                        pantallita.update(datos)
-                    cliente.close()
-        self.server.close()
+class MiTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        datos = self.request.recv(256).decode()
+        if 'Q' in datos:
+            datos = datos.replace('Q','')
+            logging.info(datos)
+            MiOled.update(datos)
 
 if __name__ == "__main__":
     logconf = { 'filename'  : 'datos.log',
@@ -97,6 +73,8 @@ if __name__ == "__main__":
                 'datefmt'   : '%c'}
                 
     logging.basicConfig(**logconf)
-    Server(port=1234).run(MiPantallita())
-
-
+    logging.info('A la escucha ...')
+    MiOled.begin()
+    HOST, PORT = "", 1234
+    server = socketserver.TCPServer((HOST, PORT), MiTCPHandler)
+    server.serve_forever()
