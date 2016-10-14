b73741e2db23434bfba078e9723f2dc24c3d1473
Sergio Soriano Peiró <sersope@gmail.com>
2016-10-14 13:53:41 +0200

    Añadida estructura de directorios para el salvado de datos

--- dataserver/sspmeteo2_dataserver.py
+++ dataserver/sspmeteo2_dataserver.py
@@ -1,15 +1,21 @@
+import os
 import socketserver
-from sspmeteo2_oled import SSPMeteoOled
 from datetime import datetime
+#~ from sspmeteo2_oled import SSPMeteoOled
 
 class Datos:
+
     datos = '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0'
 
     @classmethod
     def salvaDatos(cls, datos):
         cls.datos = datos
+        # Forma la estructura de directorios para los datos
         a = datetime.now()
-        fname = a.strftime('datos/%Y-%m-%d.dat')
+        data_dir = a.strftime('datos/%Y/%Y-%m/')
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+        fname = data_dir + a.strftime('%Y-%m-%d.dat')
         with open(fname, 'a') as f:
            f.write(a.strftime('%c, ') + cls.datos + '\n')
 
@@ -19,12 +25,12 @@ class MiTCPHandler(socketserver.BaseRequestHandler):
         if 'Q' in req:
             datos = req.replace('Q','')
             Datos.salvaDatos(datos)
-            SSPMeteoOled.update(datos)
+            #~ SSPMeteoOled.update(datos)
         elif 'GET_DATOS' in req:
             self.request.sendall(Datos.datos.encode())
 
 if __name__ == "__main__":
-    SSPMeteoOled.begin()
+    #~ SSPMeteoOled.begin()
     HOST, PORT = "", 1234
     server = socketserver.TCPServer((HOST, PORT), MiTCPHandler)
     server.serve_forever()
